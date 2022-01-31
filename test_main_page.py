6ae041d098a9d31d3bc1620929d326f20f2d10fe
Solovechik <msol294@gmail.com>
2022-01-31 22:03:26 +0500

    added class, methods and locators for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -15,3 +16,5 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
