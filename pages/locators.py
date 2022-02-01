20dc5d617644be0095a17230146dceab0c9948dd
Solovechik <msol294@gmail.com>
2022-02-01 22:33:50 +0500

    added test for product page

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
 
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BASKET_PRICE = (By.CSS_SELECTOR, '#content_inner article div:nth-child(2) p')
+    ACTUAL_PRICE = (By.CSS_SELECTOR, '#messages div:nth-child(3) div strong')
+    ADD_BOOK_MESSAGE = (By.CSS_SELECTOR, '#messages div div strong')
+    BOOK_TITLE = (By.CSS_SELECTOR, '.product_main h1')
+
---
+++ pages/product_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_good_to_basket(self):
+        self.should_be_add_button()
+        self.push_add_button()
+        self.solve_quiz_and_get_code()
+        self.title_message_correct()
+        self.price_message_correct()
+
+    def should_be_add_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), 'There is no login button'
+
+    def push_add_button(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+
+    def title_message_correct(self):
+        book_title = self.browser.find_element(*ProductPageLocators.BOOK_TITLE).text
+        add_book_message = self.browser.find_element(*ProductPageLocators.ADD_BOOK_MESSAGE).text
+        assert book_title == add_book_message, 'The title is wrong'
+
+    def price_message_correct(self):
+        price_message = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        actual_price = self.browser.find_element(*ProductPageLocators.ACTUAL_PRICE).text
+        assert price_message == actual_price, 'The price is wrong'
